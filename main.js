054f89562f00d9918b4eef55fd5dde8d248317df
MattTuring <mjtowarnicky+1@gmail.com>
2019-08-14 11:24:50 -0600

    added/corrected all js functions

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 
 
-var dogName = document.getElementsByClassName('bold')
+
 
 function nameDog() {
-var dogValue = document.getElementById('jsDogInput').value;
-dogName.innerHTML = dogValue;
+  document.getElementById('dog').innerHTML = document.getElementById('jsDogInput').value;
+  if (document.getElementById('jsDogInput').value != "") {
+    document.getElementById('jsDogInput').value = ""
+  }
   console.log("works")
 }
 
@@ -21,31 +23,50 @@ function hideTextThree() {
 }
 
 function tipOne() {
-document.getElementById('jsTipOne').classList.add('how');
-document.getElementById('jsTipOne').classList.remove('no-how');
+  document.getElementById('jsTipOne').classList.add('how');
+  document.getElementById('jsTipOne').classList.remove('no-how');
 }
 
 function noTipOne() {
-document.getElementById('jsTipOne').classList.add('no-how');
-document.getElementById('jsTipOne').classList.remove('how')
+  document.getElementById('jsTipOne').classList.add('no-how');
+  document.getElementById('jsTipOne').classList.remove('how')
 }
 
 function tipTwo() {
-document.getElementById('jsTipTwo').classList.add('how');
-document.getElementById('jsTipTwo').classList.remove('no-how');
+  document.getElementById('jsTipTwo').classList.add('how');
+  document.getElementById('jsTipTwo').classList.remove('no-how');
 }
 
 function noTipTwo() {
-document.getElementById('jsTipTwo').classList.add('no-how');
-document.getElementById('jsTipTwo').classList.remove('how')
+  document.getElementById('jsTipTwo').classList.add('no-how');
+  document.getElementById('jsTipTwo').classList.remove('how')
 }
 
 function tipThree() {
-document.getElementById('jsTipThree').classList.add('how');
-document.getElementById('jsTipThree').classList.remove('no-how');
+  document.getElementById('jsTipThree').classList.add('how');
+  document.getElementById('jsTipThree').classList.remove('no-how');
 }
 
 function noTipThree() {
-document.getElementById('jsTipThree').classList.add('no-how');
-document.getElementById('jsTipThree').classList.remove('how')
+  document.getElementById('jsTipThree').classList.add('no-how');
+  document.getElementById('jsTipThree').classList.remove('how');
+}
+
+function dogInfoText() {
+
+  var dogInfo = document.getElementById('jsDogInfo');
+  var dogSelectValue = dogInfo.options[dogInfo.selectedIndex].value;
+
+  if (dogSelectValue == "how") {
+    document.getElementById('text1').innerHTML  = document.getElementById('jsDogTextInput').value
+    console.log("how")
+  } else if ( dogSelectValue == "what" ){
+    document.getElementById('text2').innerHTML  = document.getElementById('jsDogTextInput').value
+    console.log("what")
+  } else if ( dogSelectValue == "fact") {
+    document.getElementById('text3').innerHTML  = document.getElementById('jsDogTextInput').value
+    console.log("fact")
+  } else {
+    console.log("not selected")
+  }
 }
