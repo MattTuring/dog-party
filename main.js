c94062808144ee65e60d2e78076579c88431aa75
MattTuring <mjtowarnicky+1@gmail.com>
2019-08-16 12:20:02 -0600

    removed inline event listners and moved to JS

--- main.js
+++ main.js
@@ -1,70 +1,86 @@
 
+var dogName = document.getElementById('jsNameDog');
 
-
-
-function nameDog() {
+dogName.addEventListener('click', function nameDog() {
   document.getElementById('dog').innerHTML = document.getElementById('jsDogInput').value;
   if (document.getElementById('jsDogInput').value != "") {
     document.getElementById('jsDogInput').value = ""
   }
-}
+})
 
+var hideOne = document.getElementById('jsHideTextOne');
 
-
-function hideTextOne() {
+hideOne.addEventListener('click', function hideTextOne() {
   document.getElementById('text1').classList.toggle('hide')
-}
+})
 
-function hideTextTwo() {
+var hideTwo = document.getElementById('jsHideTextTwo');
+
+hideTwo.addEventListener('click', function hideTextTwo() {
   document.getElementById('text2').classList.toggle('hide')
-}
+})
 
-function hideTextThree() {
+var hideThree = document.getElementById('jsHideTextThree');
+
+hideThree.addEventListener('click',function hideTextThree() {
   document.getElementById('text3').classList.toggle('hide')
-}
+})
 
-function tipOne() {
+
+
+var how = document.getElementById('jsHow');
+
+
+how.addEventListener('mouseover', function tipOne() {
   document.getElementById('jsTipOne').classList.add('how');
   document.getElementById('jsTipOne').classList.remove('no-how');
-}
+});
 
-function noTipOne() {
+
+how.addEventListener('mouseout', function noTipOne() {
   document.getElementById('jsTipOne').classList.add('no-how');
-  document.getElementById('jsTipOne').classList.remove('how')
-}
+  document.getElementById('jsTipOne').classList.remove('how');
+});
 
-function tipTwo() {
+
+var what = document.getElementById('jsWhat');
+
+what.addEventListener('mouseover', function tipTwo() {
   document.getElementById('jsTipTwo').classList.add('how');
   document.getElementById('jsTipTwo').classList.remove('no-how');
-}
+})
 
-function noTipTwo() {
+what.addEventListener('mouseout',function noTipTwo() {
   document.getElementById('jsTipTwo').classList.add('no-how');
   document.getElementById('jsTipTwo').classList.remove('how')
-}
+})
 
-function tipThree() {
+var fact = document.getElementById('jsFact');
+
+fact.addEventListener('mouseover',function tipThree() {
   document.getElementById('jsTipThree').classList.add('how');
   document.getElementById('jsTipThree').classList.remove('no-how');
-}
+})
 
-function noTipThree() {
+fact.addEventListener('mouseout', function noTipThree() {
   document.getElementById('jsTipThree').classList.add('no-how');
   document.getElementById('jsTipThree').classList.remove('how');
-}
+})
 
-function dogInfoText() {
+var jsDogInfoButton = document.getElementById('jsDogInfoButton');
+
+jsDogInfoButton.addEventListener('click', function dogInfoText() {
 
   var dogInfo = document.getElementById('jsDogInfo');
   var dogSelectValue = dogInfo.options[dogInfo.selectedIndex].value;
 
-  if (dogSelectValue == "how") {
+  if (dogSelectValue == 'how') {
     document.getElementById('text1').innerHTML  = document.getElementById('jsDogTextInput').value
   } else if ( dogSelectValue == "what" ){
     document.getElementById('text2').innerHTML  = document.getElementById('jsDogTextInput').value
   } else if ( dogSelectValue == "fact") {
     document.getElementById('text3').innerHTML  = document.getElementById('jsDogTextInput').value
   } else {
-    console.log("not selected")
+    console.log('not selected')
   }
-}
+})
